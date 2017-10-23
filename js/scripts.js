b87dbc9ccd26732941f0292bf624664e8c50b268
Holly Wilkalis and Sam Kissee <sam@>
2017-10-23 10:25:09 -0700

    make code DRY

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 $(function() {
   $("form#form1").submit(function(event){
   event.preventDefault();
-  var questionOne = $("input#question1").val();
-  var questionTwo = $("input#question2").val();
-  var questionThree = $("input#question3").val();
-  var questionFour = $("input#question4").val();
-  var array = [questionOne, questionTwo, questionThree, questionFour];
-  var newArray = [];
-  newArray.push(array[0]);
-  newArray.push(array[1]);
-  newArray.push(array[2]);
-  $("ul.answers").append("<li>"+ newArray[1] + "</li>");
-  $("ul.answers").append("<li>"+ newArray[0] + "</li>");
-  $("ul.answers").append("<li>"+ newArray[2] + "</li>");
+  var questions = ["question1", "question2", "question3", "question4"];
+  questions.forEach(function(question) {
+    var userInput = $("input#" + question).val();
+    var array = [userInput];
+    var newArray = [];
+    newArray.push((array[0]));
+    newArray.forEach(function(li) {
+      $("ul.answers").append("<li>"+ li + "</li>");
+    });
   });
 });
+});
+
+  // var questionOne = $("input#question1").val();
+  // var questionTwo = $("input#question2").val();
+  // var questionThree = $("input#question3").val();
+  // var questionFour = $("input#question4").val();
+  // var array = [questionOne, questionTwo, questionThree, questionFour];
+  // var newArray = [];
+  // newArray.push(array[0]);
+  // newArray.push(array[1]);
+  // newArray.push(array[2]);
+  //
+  // $("ul.answers").append("<li>"+ newArray[0] + "</li>");
+  // $("ul.answers").append("<li>"+ newArray[2] + "</li>");
